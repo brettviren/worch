3eb06afdfc7f53a0e920bee0449fb45be197c4ac
Sebastien Binet <binet@cern.ch>
2013-08-22 17:21:33 +0200

    pkgconf: more py-2.6 bwd support

--- orch/pkgconf.py
+++ orch/pkgconf.py
@@ -11,6 +11,19 @@ from subprocess import CalledProcessError
 try:
     from subprocess import check_output
 except ImportError:
+    class CalledProcessError(Exception):
+        """This exception is raised when a process run by check_call() or
+        check_output() returns a non-zero exit status.
+        The exit status will be stored in the returncode attribute;
+        check_output() will also store the output in the output attribute.
+        """
+        def __init__(self, returncode, cmd, output=None):
+            self.returncode = returncode
+            self.cmd = cmd
+            self.output = output
+        def __str__(self):
+            return "Command '%s' returned non-zero exit status %d" % (self.cmd, self.returncode)
+
     def check_output(*popenargs, **kwargs):
         r"""Run command with arguments and return its output as a byte string.
 
