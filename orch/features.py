3ddde6c5384660a52cbfe373701c4169ad3dee11
Brett Viren <bv@bnl.gov>
2013-08-30 14:18:08 -0400

    Add a cmake version of the autoconf feature.
    This has a lot of cut-and-paste overlap the should be factored out.

--- orch/features.py
+++ orch/features.py
@@ -518,6 +518,11 @@ def feature_svn(self):
 autoconf_requirements = {
     'source_dir': 'sources',
     'source_unpacked': '{package}-{version}',
+    # fixme: the prepare_script should typically match something that
+    # provides the source directory, eg, unpacked_target from
+    # tarball_requirements.  Best to make these the same keywords to
+    # make it clear.  Currently, if the package isn't truly autoconf
+    # one needs to change this "configure" value in two places.
     'prepare_script': 'configure',
     'prepare_script_options': '--prefix={install_dir}',
     'prepare_target': 'config.status',
@@ -601,6 +606,86 @@ def feature_autoconf(self):
     #              rule = 'which cmake', env=pfi.env,
     #              depends_on = 'cmake_install')
 
+
+    return
+
+
+
+cmakemake_requirements = {
+    'source_dir': 'sources',
+    'source_unpacked': '{package}-{version}',
+    'unpacked_target': 'CMakeLists.txt',
+    'prepare_cmd': '/bin/env && cmake ../../{source_dir}/{source_unpacked} -DCMAKE_INSTALL_PREFIX={install_dir}',
+    'prepare_cmd_options': '',
+    'prepare_target': 'CMakeCache.txt',
+    'build_dir': 'builds/{package}-{version}',
+    'build_cmd': 'make',
+    'build_cmd_options': None,
+    'build_target': None,
+    'install_dir': '{PREFIX}',
+    'install_cmd': 'make install',
+    'install_cmd_options': None,
+    'install_target': None,
+}
+
+@TaskGen.feature('cmakemake')
+def feature_cmakemake(self):
+    pfi = PackageFeatureInfo(self.package_name, 'cmakemake', self.bld, cmakemake_requirements)
+
+    d_source = pfi.get_node('source_dir')
+    d_unpacked = pfi.get_node('source_unpacked', d_source)
+    f_unpack_target = pfi.get_node('unpacked_target',d_unpacked)
+
+    d_build = pfi.get_node('build_dir')
+    f_prepare_result = pfi.get_node('prepare_target', d_build)
+    f_build_result = pfi.get_node('build_target', d_build)
+
+    d_prefix = pfi.get_node('install_dir')
+    f_install_result = pfi.get_node('install_target', d_prefix)
+
+    def prepare_task(task):
+        cmd = "%s %s" % (pfi.get_var('prepare_cmd'), pfi.get_var('prepare_cmd_options') or '')
+        return _worch_exec_command(task, cmd)
+        
+    self.bld(name = pfi.format('{package}_prepare'),
+             rule = prepare_task,
+             source = f_unpack_target,
+             target = f_prepare_result,
+             cwd = d_build.abspath(),
+             depends_on = pfi.get_deps('prepare'),
+             env = pfi.env)
+
+    # fixme: this is copy-and-paste from feature_autoconf().  Need more DRY!
+
+    def build_task(task):
+        cmd = "%s %s" % (
+                 pfi.get_var('build_cmd'),
+                 pfi.get_var('build_cmd_options') or '',
+            )
+        return _worch_exec_command(task, cmd)
+        
+    self.bld(name = pfi.format('{package}_build'),
+             rule = build_task,
+             source = f_prepare_result,
+             target = f_build_result,
+             cwd = d_build.abspath(),
+             depends_on = pfi.get_deps('build'),
+             env = pfi.env)
+
+    def install_task(task):
+        cmd = "%s %s" % (
+                 pfi.get_var('install_cmd'),
+                 pfi.get_var('install_cmd_options') or '',
+            )
+        return _worch_exec_command(task, cmd)
+        
+    self.bld(name = pfi.format('{package}_install'),
+             rule = install_task,
+             source = f_build_result,
+             target = f_install_result,
+             cwd = d_build.abspath(),
+             depends_on = pfi.get_deps('install'),
+             env = pfi.env)
 
     return
 
