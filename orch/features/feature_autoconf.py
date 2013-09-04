66c3b93aa4696f18ddeaac1341ca27db23ffddc8
Brett Viren <bv@bnl.gov>
2013-09-04 14:42:00 -0400

    Should not set source_unpacked here, and certainly shouldn't set it to a wrong value.

--- orch/features/feature_autoconf.py
+++ orch/features/feature_autoconf.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+'''
+This feature is a specific "prepare" feature to set Autoconf related
+configuration items.
+'''
 from .pfi import feature
 from .feature_prepare import generic
 
 requirements = {
     'unpacked_target': 'configure',
-    'source_unpacked': 'config.status',
+    'source_unpacked': None,
     'prepare_cmd': '../../{source_unpacked}/configure',
     'prepare_cmd_options': '--prefix={install_dir}',
     'prepare_target': 'config.status',
