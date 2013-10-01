28b7fff1b64884c520e17cb1b4338256ba8b88ac
Brett Viren <bv@bnl.gov>
2013-10-01 13:23:22 -0400

    This version of the test follows closely the FNAL build scripts and works.

--- tests/test_ups/mock.sh
+++ tests/test_ups/mock.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
-# Test ups declare
-# Only requirement is that UPS_DIR is set to where the "ups" binary is installed.
 
-if [ -z "$UPS_DIR" ] ; then
-    echo "Must set UPS_DIR so \$UPS_DIR/bin/ups points to 'ups' binary"
+uutarball="$1" ; shift
+PRODUCTS="$1" ; shift
+
+usage () { 
+    echo "mock.sh ups_upd-X.Y.Z-source.tar.bz2 [/path/to/directory]"
     exit 1
+}
+
+if [ -z "$uutarball" ] ; then
+    usage
 fi
-export UPS_SHELL=bash
+uutarball=$(readlink -f $uutarball)
+uu_version="$(echo $uutarball|sed -e 's|.*/ups-upd-||' -e 's|-source.tar.bz2||')"
+uu_vusstr="v$(echo $uu_version | tr '.' '_')"
+uu_xyzver="$(echo $uu_version | tr -d '.')"
+
+my_products=no
+if [ -z "$PRODUCTS" ] ; then
+    PRODUCTS=$(mktemp -d)
+    my_products=yes
+fi
+
 
 cmd () {
-    echo "Running command:" 1>&2
+    echo "Running command in $(pwd):" 1>&2
     echo "$@" 1>&2
     $@
     err=$?
     if [ "$err" != "0" ] ; then
+	env | sort
 	echo "Command failed with \"$err\"" 1>&2
+	echo $@
 	exit $err
     fi
 }
+
+export PRODUCTS
+cmd rm -rf $PRODUCTS
+cmd mkdir -p $PRODUCTS
+
+echo "Using PRODUCTS=$PRODUCTS"
 
 dump () {
     filename=$1 ; shift
@@ -28,15 +51,43 @@ dump () {
     echo 
 }
 
-PATH=$UPS_DIR/bin:$PATH
-export PRODUCTS=$(mktemp -d)
-echo "Using PRODUCTS=$PRODUCTS"
+export UPS_SHELL=bash
+
+cd $PRODUCTS
+cmd tar -xjf $uutarball
+tmp_flav="$(uname)-$(uname -r)"
+tmp_build_dir="ups/$uu_vusstr/$tmp_flav"
+cmd mkdir $tmp_build_dir
+cmd tar -xzf ups/$uu_vusstr/source/ups${uu_xyzver}.tar.gz -C $tmp_build_dir
+cd $tmp_build_dir
+tmp_ups_dir=$PRODUCTS/$tmp_build_dir 
+export UPS_DIR=$tmp_ups_dir
+cmd make all
+ups_flavor=$(./bin/ups flavor)
+cd $PRODUCTS
+
+new_ups_dir=$PRODUCTS/ups/$uu_vusstr/$ups_flavor
+echo "Pivot UPS from:"
+echo "   $tmp_ups_dir"
+echo "to"
+echo "   $new_ups_dir"
+cmd mv $tmp_ups_dir $new_ups_dir
+UPS_DIR=$new_ups_dir
+
+echo "Declare UPS"
+${UPS_DIR}/bin/ups declare ups ${uu_vusstr} -r ups/${uu_vusstr}/${ups_flavor} -4 -m ups.table -C -z ${PRODUCTS}
+${UPS_DIR}/bin/ups declare ups ${uu_vusstr} -C -c
+
+echo "Sourcing $PRODUCTS/setup"
+source $PRODUCTS/setup
+
+echo "Now mock install a package"
 
 package=hello
 version=2.8
 vusstr=v$(echo $version | tr '.' '_')
 qualifiers=debug
-flavor=$(ups flavor)
+
 #prod_root_dir="$package/$version"
 prod_root_dir="$package/$vusstr"
 prod_inst_dir="$prod_root_dir/$qualifiers"
@@ -51,10 +102,10 @@ chmod +x $PRODUCTS/$prod_inst_dir/bin/hello
 
 # fake a table file generation
 mkdir -p $PRODUCTS/$prod_root_dir/ups
-table_file=$PRODUCTS/$prod_root_dir/ups/hello.table
+table_file=$PRODUCTS/$prod_root_dir/ups/${package}.table
 cat <<EOF > $table_file
 File = Table
-Product = hello
+Product = $package
 
 Group:
   Flavor = ANY
@@ -104,4 +155,6 @@ hello
 
 #echo "Removing working directory: $PRODUCTS"
 #rm -rf $PRODUCTS
+
+
 
