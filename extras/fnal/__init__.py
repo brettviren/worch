6206bf71d2bf99011567eff6b9a1a12121af4ce8
Brett Viren <bv@bnl.gov>
2013-11-22 18:11:57 -0500

    More towards worching the Fermilab builds scripts.

--- extras/fnal/__init__.py
+++ extras/fnal/__init__.py
@@ -3,19 +3,22 @@
 waf tool for features related to installing code the "Fermilab way"
 '''
 
+
+import os
+from waflib.TaskGen import feature
+
+from orch.util import urlopen, get_unpacker
+from orch.wafutil import exec_command
+import orch.features
+
 def configure(cfg):
     pass
+
 def build(bld):
     pass
 
 
 
-from waflib.TaskGen import feature
-
-from orch.util import urlopen, get_unpacker
-from orch.wafutil import exec_command
-
-import orch.features
 orch.features.register_defaults(
     'fnalsrcbundle', 
     fnal_srcbundle_file = '{fnal_name}-{version}-source.tar.bz2',
@@ -23,7 +26,6 @@ orch.features.register_defaults(
     fnal_srcbundle_urlfile = '{package}-{version}.url',
     fnal_srcbundle_checksum = '',
 )
-
 @feature('fnalsrcbundle')
 def feature_fnalsrcbundle(tgen):
     '''
@@ -90,4 +92,36 @@ def feature_fnalsrcbundle(tgen):
               source = tgen.worch.fnal_srcbundle_file,
               target = unpack_target)
 
+    return
+
+
+
+orch.features.register_defaults(
+    'fnalbuilder', 
+    fnal_builder_dir = '{fnal_upsprod_subdir}',
+    fnal_builder_script = './build_{package}.sh',
+    fnal_builder_script_options = '',
+    fnal_builder_target = None,
+    fnal_builder_prereq = None,
+)
+@feature('fnalbuilder')
+def feature_fnalbuilder(tgen):
+
+    products_dir = tgen.make_node(tgen.worch.fnal_products_path)
+    cwd = products_dir.make_node(tgen.worch.fnal_builder_dir)
+    assert tgen.worch.fnal_builder_target,\
+        tgen.worch.format('No fnal_builder_target for {package} {version} in feature fnalbuilder')
+
+    tgt = products_dir.make_node(tgen.worch.fnal_builder_target)
+    src = list()
+    for ps in tgen.worch.fnal_builder_prereq or list():
+        p,s = ps.split('_',1)
+        src.append(tgen.control_node(s,p))
+
+    build_rule = '{fnal_builder_script} {fnal_builder_script_options}'
+    tgen.step('fnalbuilder',
+              rule = tgen.worch.format(build_rule),
+              source = src,
+              target = tgt,
+              cwd = cwd.abspath())
     return
