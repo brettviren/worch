645414d20b0d030eca9d41fbe8e2ccdfdf65781f
Brett Viren <bv@bnl.gov>
2014-09-02 09:41:57 -0400

    If patch_url is a relative path, look for it in install areas.

--- orch/features/feature_patch.py
+++ orch/features/feature_patch.py
@@ -4,6 +4,8 @@ Features to apply a patch to an unpacked source directory.
 
 It requires the 'unpack' step and provides the 'patch' step.
 '''
+import os
+import sys
 from waflib.TaskGen import feature
 import waflib.Logs as msg
 
@@ -13,24 +15,51 @@ from orch.wafutil import exec_command
 import orch.features
 orch.features.register_defaults(
     'patch',
+    patch_url = "",
     patch_urlfile = '{urlfile_dir}/{package}-{version}.patch.url',
     patch_file = '{package}-{version}.patch',
-    patch_file_path = '{patch_dir}/{patch_file}',
     patch_checksum = '',
     patch_cmd = 'patch -i',
     patch_cmd_std_opts = '',
     patch_cmd_options = '',
     )
 
+def resolve_url(url, filetype='patches'):
+    '''
+    Return a full URL given a maybe partial one.
+
+    If the URL is a relative path, look for the file in a few places.
+    '''
+    if not url: 
+        return
+    if ':' in url:
+        return url
+    if url.startswith('/'):
+        return 'file://' + url
+    trials = ['.', sys.prefix, sys.prefix+'/share/worch', sys.prefix+'/share/worch/'+filetype]
+    for maybe in trials:
+        fullpath = os.path.realpath(os.path.join(maybe, url))
+        #print 'MAYBE:',fullpath
+        if os.path.exists(fullpath):
+            return 'file://' + fullpath
+        continue
+    return                      # fail
+
 @feature('patch')
 def feature_patch(tgen):
     '''
     Apply a patch
     '''
+    w = tgen.worch
+    patch_url = resolve_url(w.patch_url)
+    if not patch_url:
+        msg = 'No patch file found (%s) for package "%s"' % (w.patch_url, w.package)
+        raise ValueError,msg
+
     tgen.step('urlpatch',
-              rule = "echo '%s' > %s" % (tgen.worch.patch_url, tgen.worch.patch_urlfile),
+              rule = "echo '%s' > %s" % (patch_url, w.patch_urlfile),
               update_outputs = True,
-              target = tgen.worch.patch_urlfile)
+              target = w.patch_urlfile)
 
     # fixme: this is mostly a cut-and-paste from feature_tarball.
     # Both should be factored out to common spot....
@@ -44,10 +73,10 @@ def feature_patch(tgen):
         except Exception:
             import traceback
             traceback.print_exc()
-            msg.error(tgen.worch.format("[{package}_dlpatch] problem downloading [{patch_urlfile}]"))
+            msg.error(w.format("[{package}_dlpatch] problem downloading [{patch_urlfile}]"))
             raise
 
-        checksum = tgen.worch.patch_checksum
+        checksum = w.patch_checksum
         if not checksum:
             return
         hasher_name, ref = checksum.split(":")
@@ -57,7 +86,7 @@ def feature_patch(tgen):
         hasher.update(tgt.read('rb'))
         data= hasher.hexdigest()
         if data != ref:
-            msg.error(tgen.worch.format("[{package}_dlpatch] invalid checksum:\nref: %s\nnew: %s" %\
+            msg.error(w.format("[{package}_dlpatch] invalid checksum:\nref: %s\nnew: %s" %\
                                         (ref, data)))
             try:
                 os.remove(tgt.abspath())
@@ -67,22 +96,22 @@ def feature_patch(tgen):
         return
     tgen.step('dlpatch',
               rule = dl_task,
-              source = tgen.worch.patch_urlfile,
-              target = tgen.worch.patch_file)
+              source = w.patch_urlfile,
+              target = w.patch_file)
 
     def apply_patch(task):
         src = task.inputs[0].abspath()
         tgt = task.outputs[0].abspath()
-        cmd = "%s %s %s" % ( tgen.worch.patch_cmd, src, tgen.worch.patch_cmd_options )
+        cmd = "%s %s %s" % ( w.patch_cmd, src, w.patch_cmd_options )
         ret = exec_command(task, cmd)
         return ret
 
-    after =  tgen.worch.package + '_unpack'
-    before = tgen.worch.package + '_prepare'
+    after =  w.package + '_unpack'
+    before = w.package + '_prepare'
 
     tgen.step('patch',
               rule = apply_patch,
-              source = [tgen.worch.patch_file, tgen.control_node('unpack')],
+              source = [w.patch_file, tgen.control_node('unpack')],
               target = [tgen.control_node('patch')],
               depends_on = [after],
               after = [after], before = [before])
