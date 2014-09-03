21e01e7bc9a81d09cdc599cfa25bc393b95f313b
Brett Viren <bv@bnl.gov>
2014-09-03 12:57:45 -0400

    Install examples and extras.

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+import os
 from setuptools import setup, find_packages
 setup(name = 'worch',
-      version = '1.2',
+      version = '1.2.1',
       description = 'Build orchestration with waf.',
       author = 'Brett Viren',
       author_email = 'brett.viren@gmail.com',
       license = 'GPLv2',
       url = 'http://github.com/brettviren/worch',
       py_modules = ['orchlib'],
-      packages = ['orch', 'orch.features'],
-      include_package_data = True,
+      packages = ['orch', 'orch.features', 'worch', 'worch.extras'],
       scripts = ['waf'],
+      data_files = [
+          ('share/worch/wscripts/worch', ['wscript']),
+          ] + [('share/worch/config/'+x[0],
+                map(lambda y: x[0]+'/'+y, x[2])) for x in os.walk('examples')],
 )
