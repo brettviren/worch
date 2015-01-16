70d53f06da40b166d918ea02fc8ac48dbee5c639
Brett Viren <bv@bnl.gov>
2015-01-16 12:52:56 -0500

    Version to 1.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 setup(name = 'worch',
-      version = '1.2.1-dev',
+      version = '1.3.0',
       description = 'Build orchestration with waf.',
       author = 'Brett Viren',
       author_email = 'brett.viren@gmail.com',
