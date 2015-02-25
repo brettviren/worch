a0a1e167a32592e778d8ae79d125d462ab08faf0
Brett Viren <bv@bnl.gov>
2015-02-25 16:40:48 -0500

    Allow downloads to be made from a list of URLs to provide fail over mirrors.

--- tests/test_download.py
+++ tests/test_download.py
@@ -11,5 +11,16 @@ def test_python():
     tgt = os.path.basename(url)
     download(url,tgt, md5sum)
 
+def test_boost_many():
+    urls = [
+        'http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.gz',
+        'https://lbne.bnl.gov/packages/source/boost_1_57_0.tar.gz',
+        ]
+    md5sum = 'md5:25f9a8ac28beeb5ab84aa98510305299'
+    tgt = os.path.basename(urls[0])
+    got = download(urls, tgt, md5sum)
+    print got
+
 if '__main__' == __name__:
-    test_python()
+#    test_python()
+    test_boost_many()
