64601a0e76e7c74fad7a18ba84bda0c47ebe31cc
Brett Viren <brett.viren@gmail.com>
2015-03-12 13:46:01 -0400

    Go to 1.3.3-dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 setup(name = 'worch',
-      version = '1.3.3',
+      version = '1.3.3-dev',
       description = 'Build orchestration with waf.',
       author = 'Brett Viren',
       author_email = 'brett.viren@gmail.com',
